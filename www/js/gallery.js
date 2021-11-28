282591c0d6b7c2518c14dc5e329826fce6ed31f3
Parker Jones <pwjones1997@gmail.com>
2021-11-28 15:56:44 -0600

    Beginning to make Gallery Page more dynamic - Adding a js file to handle the gallery page loading logic - Include gallery.js in gallery.html - Remove placeholder <img> from gallery.html - Adding /ext dir for a couple test images

---
+++ www/js/gallery.js
@@ -0,0 +1,18 @@
+document.onload = () => {
+	// Based on the gallery we are accessing, localhost:6969/gallery.html?id=1
+	// Retrieve the photos to display on page, providing a gallery ID
+
+	// Populate page with header photo, collections, & collection photos
+	loadHeader();
+	loadCollections();
+}
+
+// TODO: Loads the Gallery Title, Date, Owner, and Header Photo
+function loadHeader() {
+
+}
+
+// TODO: Loads all the Gallery's Collection names and subsequent photos
+function loadCollections() {
+
+}
