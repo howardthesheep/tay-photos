c12c65ec565a0fd2cb7cb53991c4d202cf9d5893
Parker Jones <pjones@ellem.ai>
2022-04-05 23:26:33 -0500

    Adding Gallery frontend functionality - Creating gallery specific CSS page - Removing printing requests to fileserver on backend - Add image lightboxing when clicked in gallery - Disable contextmenu in gallery - Begin adding button menu to image lightbox

--- server/server.go
+++ server/server.go
@@ -14,7 +14,7 @@ func main() {
 // Extra logic on top of the /www file server to ensure authentication, logging, etc.
 func fileServerMiddleware(fs http.Handler) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		println(requestString(request))
+		//println(requestString(request))
 
 		// TODO: Ensure authentication for access to gallery editor pages & stuff
 
--- www/js/gallery.js
+++ www/js/gallery.js
@@ -5,6 +5,11 @@ const api = new ApiManager();
 // Populate page with header photo, collections, & collection photos
 await loadGalleryData();
 
+// Disable right clicking on the page, preventing ~most~ user's from saving
+// images easily. Although any dev worth their salt can figure out how to
+// download them
+disableRightClick();
+
 // Loads all of the dynamic gallery data and populates the page with it
 async function loadGalleryData() {
 	let galleryInfo, galleryPhotos;
@@ -29,6 +34,7 @@ async function loadGalleryData() {
 	updateGalleryPhotos(galleryPhotoJson);
 }
 
+// Dynamically updates page information with data from backend
 function updateGalleryInfo(json) {
 	let date = new Date(json.create_time);
 
@@ -36,10 +42,9 @@ function updateGalleryInfo(json) {
 	document.querySelector("#gallery-date").textContent = date.toDateString();
 }
 
+// Dynamically populates the page with gallery section and associated images
 function updateGalleryPhotos(json) {
 	const collections = parsePhotoCollections(json);
-
-	console.log(collections);
 
 	for (const collection of collections) {
 		// Create section header
@@ -55,6 +60,7 @@ function updateGalleryPhotos(json) {
 			const img = document.createElement("img");
 			img.src = api.apiLocation + "/gallery/photo?id=" + photoId
 			img.alt = "Gallery Image"
+			img.onclick = lightboxImage;
 			section.append(img)
 		}
 
@@ -62,6 +68,60 @@ function updateGalleryPhotos(json) {
 		document.body.append(sectionTitle);
 		document.body.append(section);
 	}
+}
+
+// Shows an image in a fullscreen view overlayed on top of the current page
+function lightboxImage(evt) {
+	const imageSource = evt.target.src;
+
+	// Create our image itself
+	const lightboxImage = document.createElement("img");
+	lightboxImage.src = imageSource;
+	lightboxImage.alt = "Gallery Image";
+	lightboxImage.onclick = removeLightbox;
+	lightboxImage.classList.add("lightboxImage");
+
+	// Add Share button
+	const shareBtn = document.createElement("button");
+	shareBtn.textContent = "Share";
+	shareBtn.onclick = (evt) => {}; // TODO: Implement Sharing feature
+	shareBtn.classList.add("lightboxButton");
+
+	// Add download button
+	const downloadBtn = document.createElement("button");
+	downloadBtn.textContent = "Download";
+	downloadBtn.onclick = (evt) => {}; // TODO: Implement Downloading feature
+	downloadBtn.classList.add("lightboxButton");
+
+	// Create Buttons container
+	const buttonContainer = document.createElement("div");
+	buttonContainer.classList.add("lightboxButtonContainer");
+	buttonContainer.append(shareBtn, downloadBtn);
+
+	// Create image container
+	const imageContainer = document.createElement("div");
+	imageContainer.classList.add("lightboxContainer");
+	imageContainer.onclick = removeLightbox;
+	imageContainer.append(lightboxImage);
+	imageContainer.append(buttonContainer);
+
+	document.body.append(imageContainer);
+}
+
+// Removes all .lightboxContainer from the DOM tree
+function removeLightbox(evt) {
+	console.log(evt.target.nodeName);
+	if (evt.target.nodeName.toLowerCase() === "button") return;
+
+	const containers = document.querySelectorAll(".lightboxContainer");
+	for (const container of containers) {
+		container.remove();
+	}
+}
+
+// Disable right-clicking anywhere on the <body> or on its descendants
+function disableRightClick() {
+	document.body.oncontextmenu = () => {return false};
 }
 
 // Parses json into groups of array of json objects of the form
